8fefa3c49a81c86895738b1e867486824a9c6a89
Роман Воробец <vorobetsroman@mail.ru>
2021-03-13 21:25:49 +0300

    mask is created

--- TestIssue/Controller/VcLoginScreen.swift
+++ TestIssue/Controller/VcLoginScreen.swift
@@ -10,7 +10,13 @@ import Alamofire
 
 class VcLoginScreen: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var ui_tfPhone: UITextField!
+//    var phoneMaskSuffix = ""
+    var phoneMaskPrefix = ""
+    var phoneMask = ""
     
+    
+    
+    //===============================================
     override func viewWillAppear(_ animated: Bool) {
 
         let request = AF.request("http://dev-exam.l-tech.ru/api/v1/phone_masks", method: .get)
@@ -20,7 +26,13 @@ class VcLoginScreen: UIViewController, UITextFieldDelegate {
                 print(data.result)
                 if let result = value as? [String: Any] {
                     if let phoneMask = result["phoneMask"] as? String {
-                        print(phoneMask)
+                        if let spaceIndex = phoneMask.firstIndex(of: " ") {
+                            self.phoneMaskPrefix = String(phoneMask.prefix(through: spaceIndex))
+                            self.ui_tfPhone.text = self.phoneMaskPrefix
+//                            self.phoneMaskSuffix = String(phoneMask.suffix(from: spaceIndex))
+                            self.phoneMask = phoneMask
+//                            print(phoneMask)
+                        }
                     }
                 }
         
@@ -28,35 +40,64 @@ class VcLoginScreen: UIViewController, UITextFieldDelegate {
                 print(error)
             }
         }
-        
         ui_tfPhone.delegate = self
     }
 
+    
+    
+    //===============================================
     @IBAction func ui_btSignin_tui(_ sender: UIButton) {
         performSegue(withIdentifier: "loginToLibrary", sender: Any?.self)
     }
     
+    
+    
+    //===============================================
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
-   
+    
+    
+    // MARK: textFieldDelegate
+    //===============================================
     func formattedNumber(number: String) -> String {
-        let cleanPhoneNumber = number.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
-        let mask = "## ### ###"
         var result = ""
-        var index = cleanPhoneNumber.startIndex
-        for ch in mask where index < cleanPhoneNumber.endIndex {
-            if ch == "#" {
-                result.append(cleanPhoneNumber[index])
-                index = cleanPhoneNumber.index(after: index)
-            } else {
+        var index = number.startIndex
+        
+        for ch in self.phoneMask where index < number.endIndex {
+            switch ch {
+            case "+", "0", "1"..."9" : //проходим заголовок
                 result.append(ch)
+                index = number.index(after: index)
+                break
+            case "Х" : //если в маске цифра ищем следующую цифру в number
+                while index < number.endIndex && !(number[index].isNumber) {
+                    index = number.index(after: index)
+                }
+                if index < number.endIndex {
+                    result.append(number[index])
+                    index = number.index(after: index)
+                }
+                break
+            default:
+                result.append(ch)
+                if !number[index].isNumber { //пропускаем символ в номере
+                    index = number.index(after: index)
+                }
+                break
             }
+        }
+        
+        if result.count < self.phoneMaskPrefix.count {
+            result = self.phoneMaskPrefix
         }
         return result
     }
     
+    
+    
+    //===============================================
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let text = textField.text else { return false }
         let newString = (text as NSString).replacingCharacters(in: range, with: string)
