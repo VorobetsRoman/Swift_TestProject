54473b2e95236009bd81dd042151690e01fb796d
Роман Воробец <vorobetsroman@mail.ru>
2021-03-19 11:42:25 +0300

    Added class for wares description controller

---
+++ TestIssue/ViewController/VcWarezDesc.swift
@@ -0,0 +1,29 @@
+//
+//  VcWarezDesc.swift
+//  TestIssue
+//
+//  Created by Роман Воробец on 19.03.2021.
+//
+
+import UIKit
+
+class VcWarezDesc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
